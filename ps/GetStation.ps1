d31df921e36423defa239852a1025bfe93ede1c5
Takami Kitabayashi <kitabayashi@contentsworks.co.jp>
2020-11-05 20:51:31 +0900

    クレジット追加

--- ps/GetStation.ps1
+++ ps/GetStation.ps1
@@ -1,3 +1,8 @@
+<#
+郵便番号を入力すると最寄り駅名を返します。
+このスクリプトは「HeartRails Geo API」(https://geoapi.heartrails.com/api.html)を利用しています
+#>
+
 Param($postal)
 
 $ApiEndPoint = "http://geoapi.heartrails.com/api/json?method=getStations&postal=${postal}"
